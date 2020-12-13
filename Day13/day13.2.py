18a20a3fc5783b3658e1159804cbe4e52b00191c
Akash Vacher <avacher@linkedin.com>
2020-12-13 23:48:43 +0530

    Refactored day13 part2 to have more explicit logic

--- Day13/day13.2.py
+++ Day13/day13.2.py
@@ -1,17 +1,31 @@
+from math import gcd
+
+
 def part2():
+    def lcm(a, b):
+        return abs(a * b) // gcd(a, b)
 
     with open("in.txt") as f:
         _ = f.readline()
-        buses = f.readline()
-    buses_and_delay = [(int(b), d) for d, b in enumerate(buses.split(",")) if b != "x"]
+        bus_ids = f.readline()
+    bus_ids_and_delay = [
+        (int(bus_id), delay)
+        for delay, bus_id in enumerate(bus_ids.split(","))
+        if bus_id != "x"
+    ]
 
-    bus, delay = buses_and_delay[0]
-    t = bus - delay  # By definiton, bus*1 is a multiple of (t+delay)
-    increment = bus  # Safe increment value to increase t by
-    for bus, delay in buses_and_delay[1:]:
-        while (t + delay) % bus != 0:
+    bus_id, delay = bus_ids_and_delay[0]
+    t = bus_id - delay  # This t is suitable as bus_id*1 is a multiple of (t+delay)
+    increment = bus_id  # Safe increment value to increase t with
+    for bus_id, delay in bus_ids_and_delay[1:]:
+        # Find the next value of t which is suitable for this bus_id
+        # by repeatedly incrementing it
+        while (t + delay) % bus_id != 0:
             t += increment
-        increment *= bus
+        # The new safe increment value has to be a multiple of this bus_id
+        # AND maintain that it is a multiple of all the bus_ids that came before this one
+        # Hence, we take the LCM of this bus_id with existing increment to get the new increment
+        increment = lcm(increment, bus_id)
     print(t)
 
 
