c62cb5c9e8297edb78e25b361c48d73375c6a986
Akash Vacher <avacher@linkedin.com>
2020-12-15 22:15:33 +0530

    Added Day15 solutions

---
+++ Day15/day15.1.py
@@ -0,0 +1,23 @@
+def part1():
+    d = {}
+    index = 1
+
+    def get_next(last, d, index):
+        if last not in d:
+            d[last] = index
+            return 0, d, index + 1
+        diff = index - d[last]
+        d[last] = index
+        return diff, d, index + 1
+
+    seed = list(map(int, open("in.txt").read().split(",")))
+    for item in seed[:-1]:
+        d[item] = index
+        index += 1
+    previous = seed[-1]
+    while index < 2020:
+        previous, d, index = get_next(previous, d, index)
+    print(previous)
+
+
+part1()
